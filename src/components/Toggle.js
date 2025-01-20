ea22b4ab4babbc45f62587b99454328f0b848f5a
eddy <eddy.mwaniki.27@gmail.com>
2025-01-20 15:51:48 +0300

    modifies Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  function handleCick(){
+    setIsOn((isOn) => !isOn )
+  }
+  return <button onClick={handleCick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
